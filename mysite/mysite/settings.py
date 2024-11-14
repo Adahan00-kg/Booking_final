10b615441f6d48bdb70c90bc33d41b8f4c75c49a
Adahan00-kg <20ikramidinovrinat07@gmail.com>
2024-11-14 13:39:40 +0600

    final02

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+
+from django.conf.global_settings import MEDIA_ROOT, STATIC_ROOT
 from dotenv import load_dotenv
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,6 +149,7 @@ MODELTRANSLATION_LANGUAGES = ('en','ru')
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfile/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
